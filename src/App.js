1f05bfcd0be04694055cd77f2e1d018290d74b3e
DongGeun974 <zxcasd3004@naver.com>
2023-11-17 22:03:24 +0900

    done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import Hero from "./components/Hero";
-import Analyics from "./components/Analyics";
+import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
         <Hero/>
-        <Analyics/>
+        <Analytics/>
+      <Newsletter />
+        <Cards/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Newsletter = () => {
+    return (
+        <div className='w-full py-16 text-white px-4'>
+            <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3 '>
+                <div className='lg:col-span-2 my-4'>
+                    <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>want tips & tricks to optimize your flow</h1>
+                    <p>sign up to our newletter and stay up to date.</p>
+                </div>
+                <div className='my-4'>
+                    <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
+                        <input className='p-3 flex w-full rounded-md text-black' type={"email"} placeholder='Enter email'/>
+                        <button className='text-black bg-[#00df9a] w-[200px] rounded-md font-medium my-6 ml-4 px-6 py-3 '>Notify </button>
+                    </div>
+                <p>we care bout the protection of your data. read our <span className='text-[#00df9a]'>privacy policy.</span> </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Newsletter;
